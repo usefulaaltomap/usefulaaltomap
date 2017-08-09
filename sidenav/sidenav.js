d8e6d1d1533eb9982ece918c2eec0edbc19b8323
Richard Darst <rkd@zgib.net>
2017-08-09 23:04:00 +0300

    Don't show redundant names

--- sidenav/sidenav.js
+++ sidenav/sidenav.js
@@ -9,6 +9,17 @@ angular.module('usefulAaltoMap')
 
   $scope.get_lang = utils.get_lang;
 
+  $scope.name = utils.get_lang(object, 'name');
+  var name = $scope.name;
+  var name_en = utils.get_lang(object, 'name', 'en');
+  var name_fi = utils.get_lang(object, 'name', 'fi');
+  var name_sv = utils.get_lang(object, 'name', 'sv');
+  if (name != name_en) $scope.name_en = name_en;
+  if (name != name_fi) $scope.name_fi = name_fi;
+  if (name != name_sv && name_sv != name_fi && name_sv != name_en)
+    $scope.name_sv = utils.get_lang(object, 'name', 'sv');
+
+
   $scope.selectChild = function(id) {
 
   }
